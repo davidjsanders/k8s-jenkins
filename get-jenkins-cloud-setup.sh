20b9ba9366fcd811b33ff69e6e4bbfab1d0cf4fe
David Sanders <dsanderscanada@gmail.com>
2019-07-30 21:42:48 -0400

    Fix paths in openssl command

--- get-jenkins-cloud-setup.sh
+++ get-jenkins-cloud-setup.sh
@@ -6,7 +6,11 @@ mkdir -p /tmp/jenkins
 echo $ca_crt > /tmp/jenkins/ca.crt
 echo $client_crt > /tmp/jenkins/client.crt
 echo $client_key > /tmp/jenkins/client.key
-openssl pkcs12 -export -out cert.pfx -inkey client.key -in client.crt -certfile ca.crt
+openssl pkcs12 -export \
+    -out /tmp/jenkins/cert.pfx \
+    -inkey /tmp/jenkins/client.key \
+    -in /tmp/jenkins/client.crt \
+    -certfile /tmp/jenkins/ca.crt
 
 echo
 echo "ca_crt    : $ca_crt"
