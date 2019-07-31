9986e083f2cead4f0b74d2454950144890d1039c
David Sanders <dsanderscanada@gmail.com>
2019-07-30 21:47:44 -0400

    Change pipe to tee

--- get-jenkins-cloud-setup.sh
+++ get-jenkins-cloud-setup.sh
@@ -3,9 +3,9 @@ ca_crt=$(cat ~/.kube/config | grep certificate-authority-data | cut -d':' -f2 | 
 client_crt=$(cat ~/.kube/config | grep client-certificate-data | cut -d':' -f2 | tr -d '[:space:]' | base64 -d)
 client_key=$(cat ~/.kube/config | grep client-key-data | cut -d':' -f2 | tr -d '[:space:]' | base64 -d)
 mkdir -p /tmp/jenkins
-echo $ca_crt > /tmp/jenkins/ca.crt
-echo $client_crt > /tmp/jenkins/client.crt
-echo $client_key > /tmp/jenkins/client.key
+echo $ca_crt | tee /tmp/jenkins/ca.crt
+echo $client_crt | tee /tmp/jenkins/client.crt
+echo $client_key | tee /tmp/jenkins/client.key
 openssl pkcs12 -export \
     -out /tmp/jenkins/cert.pfx \
     -inkey /tmp/jenkins/client.key \
