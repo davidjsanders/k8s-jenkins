3feb05dc16c94bb4b6e591f7a6ad6f7fd3c4138d
David Sanders <dsanderscanada@gmail.com>
2019-07-30 21:41:22 -0400

    Update jenkins setup to produce pfx

--- get-jenkins-cloud-setup.sh
+++ get-jenkins-cloud-setup.sh
@@ -2,6 +2,12 @@
 ca_crt=$(cat ~/.kube/config | grep certificate-authority-data | cut -d':' -f2 | tr -d '[:space:]' | base64 -d)
 client_crt=$(cat ~/.kube/config | grep client-certificate-data | cut -d':' -f2 | tr -d '[:space:]' | base64 -d)
 client_key=$(cat ~/.kube/config | grep client-key-data | cut -d':' -f2 | tr -d '[:space:]' | base64 -d)
+mkdir -p /tmp/jenkins
+echo $ca_crt > /tmp/jenkins/ca.crt
+echo $client_crt > /tmp/jenkins/client.crt
+echo $client_key > /tmp/jenkins/client.key
+openssl pkcs12 -export -out cert.pfx -inkey client.key -in client.crt -certfile ca.crt
+
 echo
 echo "ca_crt    : $ca_crt"
 echo "client_crt: $client_crt"
