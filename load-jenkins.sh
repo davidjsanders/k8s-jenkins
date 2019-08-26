2138feb94999d280a1193ec2399d8cd01d01f3d1
David Sanders <dsanderscanada@gmail.com>
2019-08-26 19:11:57 -0400

    Expand error text

--- load-jenkins.sh
+++ load-jenkins.sh
@@ -42,7 +42,8 @@ if [ -z "$domain_name" ]
 then
     short_banner "domain_name *NOT* found; unable to continue."
     short_banner "Rerun setting domain_name=\".thedomain.com\" before running"
-    short_banner "Note: no . is added between the service name and the domain; include it if you need it before running"
+    short_banner "Note: no . is added between the service name and the domain; "
+    short_banner "      include it if you need it, e.g. .mydomain.com"
     exit 1
 fi
 
